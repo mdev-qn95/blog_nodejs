3cfa8a75848323a920c6ff4330a36fac9d251435
unknown <thaivanphu.qn95@gmail.com>
2020-12-31 08:46:48 -0800

    [CRUD] Create new course

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -10,6 +10,20 @@ class CourseController {
       )
       .catch(next);
   }
+  // [GET] /course/create
+  create(req, res, next) {
+    res.render('courses/create');
+  }
+  // [POST] /course/store
+  store(req, res, next) {
+    const formData = req.body;
+    formData.image = `https://img.youtube.com/vi/${req.body.videoId}/sddefault.jpg`;
+    const course = new Course(req.body);
+    course
+      .save()
+      .then(() => res.redirect('/'))
+      .catch((error) => {});
+  }
 }
 
 module.exports = new CourseController();
