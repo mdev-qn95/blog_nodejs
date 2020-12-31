f88f189e47d0afda8b87e3ce4d6c8e9f828a04c6
unknown <thaivanphu.qn95@gmail.com>
2020-12-31 03:03:20 -0800

    Detail page

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,12 @@
-const newsRouter = require('./news')
-const siteRouter = require('./site')
+const newsRouter = require('./news');
+const siteRouter = require('./site');
+const coursesRouter = require('./courses');
 
 function route(app) {
+  app.use('/news', newsRouter);
+  app.use('/courses', coursesRouter);
 
-    app.use('/news', newsRouter)
-
-    app.use('/', siteRouter)
-    
+  app.use('/', siteRouter);
 }
 
-module.exports = route
+module.exports = route;
